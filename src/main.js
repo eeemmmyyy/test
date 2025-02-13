c7a913cc5e099d04deff40683c300ead7ce05aa1
emy <emili740@mail.ru>
2025-02-13 20:59:42 +0500

    slider + fix pathes commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const swiper = new Swiper('.swiper');
+const buttons = document.querySelectorAll('.slider__button');
+buttons.forEach((button, index) => {
+    button.addEventListener('mouseenter', () => {
+        swiper.slideTo(index);
+    });
+});
